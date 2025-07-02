d1fae96b730ed3e26360331ba790e3c88edbbec1
programmersEmperor <mutasim.business2020@gmail.com>
2025-07-02 21:44:48 +0300

    chore: install shadcn/ui components

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,9 @@
+import { ReactNode } from "react"
+
+type Props = {
+    children: ReactNode;
+}
+
+export default function AuthLayout({children}: Props){
+    return <div className="auth-layout">{children}</div>
+}
---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,5 @@
+import AuthForm from "@/components/authForm";
+
+export default function page(){
+    return <AuthForm type="sign-up"/>
+}
