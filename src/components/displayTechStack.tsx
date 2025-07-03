a2b5894fd193c3eb8b6096ad5a14d5662837255e
programmersEmperor <mutasim.business2020@gmail.com>
2025-07-03 20:16:57 +0300

    ui: make the tech stack icon overlap each other

--- src/components/displayTechStack.tsx
+++ src/components/displayTechStack.tsx
@@ -1,4 +1,4 @@
-import { getTechLogos } from "@/lib/utils";
+import { cn, getTechLogos } from "@/lib/utils";
 import Image from "next/image";
 
 const DisplayTechStack = async ({techStack}: TechIconProps)=>{
@@ -6,7 +6,7 @@ const DisplayTechStack = async ({techStack}: TechIconProps)=>{
     return <div className="flex">
         {
             techIcons.slice(0, 3).map(({tech, url}, index)=>{
-                return <div key={tech} className="group bg-dark-300 rounded-full p-2 flex-center">
+                return <div key={tech} className={cn("group bg-dark-300 rounded-full p-2 flex-center", index >= 1 && '-ms-3')}>
                     <span className="tech-tooltip">{tech}</span>
                     <Image src={url} className="size-5" alt={tech} width={100} height={100} />
                 </div>
