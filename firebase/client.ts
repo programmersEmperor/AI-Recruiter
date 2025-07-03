029da1327227879cf3594d9656e91a59fa019f39
programmersEmperor <mutasim.business2020@gmail.com>
2025-07-03 21:11:54 +0300

    db: setup client firestore

--- firebase/client.ts
+++ firebase/client.ts
@@ -1,6 +1,7 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from 'firebase/firestore';
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,6 +17,11 @@ const firebaseConfig = {
   measurementId: "G-1LF681WMFD"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+// CHECK TO ENSURE ONLY 1 APP IS INITIALIZED
+const app = !getApps.length 
+        ? initializeApp(firebaseConfig) 
+        : getApp()
+        
+export const auth = getAuth(app);
+export const firestore = getFirestore(app);
