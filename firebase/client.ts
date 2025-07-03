b431534add236fecfab19fee2297a9836c11d81b
programmersEmperor <mutasim.business2020@gmail.com>
2025-07-03 20:46:41 +0300

    chore: install firebase

---
+++ firebase/client.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBoJ21bfuxlzeCxj-Jf5Ly5R_Dq9ojUB6g",
+  authDomain: "notification-4d647.firebaseapp.com",
+  projectId: "notification-4d647",
+  storageBucket: "notification-4d647.firebasestorage.app",
+  messagingSenderId: "254904878310",
+  appId: "1:254904878310:web:7991a0d43b856dae232727",
+  measurementId: "G-1LF681WMFD"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
