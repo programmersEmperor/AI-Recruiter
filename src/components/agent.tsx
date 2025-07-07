dd2d92a3e756c407fd0a382b8494adc8f890a6ed
programmersEmperor <mutasim.business2020@gmail.com>
2025-07-07 23:39:04 +0300

    fix: remove NEXT_PUBLIC_VAPI_ASSISTANT_ID

--- src/components/agent.tsx
+++ src/components/agent.tsx
@@ -120,7 +120,7 @@ const Agent = ({
       setCallStatus(CallStatus.CONNECTING);
 
       if (type === "generate") {
-        await vapi.start(process.env.NEXT_PUBLIC_VAPI_ASSISTANT_ID!, undefined, undefined, process.env.NEXT_PUBLIC_VAPI_WORKFLOW_ID!, {
+        await vapi.start(undefined, undefined, undefined, process.env.NEXT_PUBLIC_VAPI_WORKFLOW_ID!, {
         
           variableValues: {
             username: userName,
